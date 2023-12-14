64aefbad939f739825f783a1b93f0ea1ba1ed21b
Fardeen Idrus <fardeenidrus123@gmail.com>
2023-12-14 23:10:54 +0000

    "change seeder"

--- tasks/management/commands/seed.py
+++ tasks/management/commands/seed.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
-
-from tasks.models import User
-
+from tasks.models import User, Teams, Board, TaskList, Task
 import pytz
 from faker import Faker
 from random import randint, random
+from django.utils import timezone
+
+fake = Faker('en_GB')
 
 user_fixtures = [
     {'username': '@johndoe', 'email': 'john.doe@example.org', 'first_name': 'John', 'last_name': 'Doe'},
@@ -25,7 +26,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.create_users()
-        self.users = User.objects.all()
+        self.create_teams_boards_tasks()
 
     def create_users(self):
         self.generate_user_fixtures()
@@ -37,7 +38,7 @@ class Command(BaseCommand):
 
     def generate_random_users(self):
         user_count = User.objects.count()
-        while  user_count < self.USER_COUNT:
+        while user_count < self.USER_COUNT:
             print(f"Seeding user {user_count}/{self.USER_COUNT}", end='\r')
             self.generate_user()
             user_count = User.objects.count()
@@ -49,12 +50,12 @@ class Command(BaseCommand):
         email = create_email(first_name, last_name)
         username = create_username(first_name, last_name)
         self.try_create_user({'username': username, 'email': email, 'first_name': first_name, 'last_name': last_name})
-       
+
     def try_create_user(self, data):
         try:
             self.create_user(data)
-        except:
-            pass
+        except Exception as e:
+            print(f"Error creating user: {e}")
 
     def create_user(self, data):
         User.objects.create_user(
@@ -64,9 +65,32 @@ class Command(BaseCommand):
             first_name=data['first_name'],
             last_name=data['last_name'],
         )
+        
+    def create_teams_boards_tasks(self):
+        for i in range(10):  
+            team = Teams.objects.create(author=User.objects.first())
+            board_name = f"Board_{i}"  # unique name for each board
+            board = Board.objects.create(author=User.objects.first(), board_name=board_name, team=team)
+            print(f"Created board '{board_name}' for team {team}")
+            # Create task lists
+            for list_name in ['To-Do', 'In Progress', 'Done']: 
+                task_list = TaskList.objects.create(board=board, listName=list_name)
+                print(f"Created task list '{list_name}' for board '{board_name}'")
+
+                #create task
+                for _ in range(15): 
+                    task = Task.objects.create(
+                        list=task_list,
+                        task_name=fake.sentence(),
+                        task_description=fake.paragraph(),
+                        due_date=fake.date_time_this_year(tzinfo=timezone.utc)
+                    )
+                    print(f"Created task '{task.task_name}' for task list '{list_name}' in board '{board_name}'")
 
 def create_username(first_name, last_name):
     return '@' + first_name.lower() + last_name.lower()
 
+
 def create_email(first_name, last_name):
     return first_name.lower() + '.' + last_name.lower() + '@example.org'
+
