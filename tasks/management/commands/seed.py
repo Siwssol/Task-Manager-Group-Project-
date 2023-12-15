93f24134c5e351889959e1a29e08d2cf1f85a698
Fardeen Idrus <fardeenidrus123@gmail.com>
2023-12-15 09:42:55 +0000

    "seeder and fix front end"

--- tasks/management/commands/seed.py
+++ tasks/management/commands/seed.py
@@ -4,6 +4,7 @@ import pytz
 from faker import Faker
 from random import randint, random
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 fake = Faker('en_GB')
 
@@ -65,28 +66,34 @@ class Command(BaseCommand):
             first_name=data['first_name'],
             last_name=data['last_name'],
         )
-        
+    
+    
     def create_teams_boards_tasks(self):
-        for i in range(10):  
-            team = Teams.objects.create(author=User.objects.first())
-            board_name = f"Board_{i}"  # unique name for each board
-            board = Board.objects.create(author=User.objects.first(), board_name=board_name, team=team)
-            print(f"Created board '{board_name}' for team {team}")
-            # Create task lists
-            for list_name in ['To-Do', 'In Progress', 'Done']: 
-                task_list = TaskList.objects.create(board=board, listName=list_name)
-                print(f"Created task list '{list_name}' for board '{board_name}'")
+        users = get_user_model().objects.all()
 
-                #create task
-                for _ in range(15): 
-                    task = Task.objects.create(
-                        list=task_list,
-                        task_name=fake.sentence(),
-                        task_description=fake.paragraph(),
-                        due_date=fake.date_time_this_year(tzinfo=timezone.utc)
-                    )
-                    print(f"Created task '{task.task_name}' for task list '{list_name}' in board '{board_name}'")
+        for user in users:
+            for i in range(10):
+                # Create a new team for each board
+                team = Teams.objects.create(author=user)
 
+                board_name = f"{user.username}'s Board {i}"
+                board = Board.objects.create(author=user, board_name=board_name, team=team)
+
+                for list_name in ['To-Do', 'In Progress', 'Done']:
+                    task_list = TaskList.objects.create(board=board, listName=list_name)
+
+                    # Create 30 tasks per board
+                    for _ in range(30):
+                        Task.objects.create(
+                            list=task_list,
+                            task_name=fake.sentence(),
+                            task_description=fake.paragraph(),
+                            due_date=fake.date_time_this_year(tzinfo=timezone.utc)
+                        )
+            print(f"Created teams, boards, and tasks for user: {user}")
+
+
+    
 def create_username(first_name, last_name):
     return '@' + first_name.lower() + last_name.lower()
 
